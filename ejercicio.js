2e66aa024502df1f47476d3fdf66c042b11c51ab
Felipe Trincado <f.trincado7@gmail.com>
2022-05-31 12:26:24 -0400

    mod2_clase1_enclases

---
+++ ejercicio.js
@@ -0,0 +1,12 @@
+let nombre = prompt ('Bienvenido terricola, ¿cual es tu nombre?');
+nombre = (nombre)
+
+console.log ('Saludos ' +nombre);
+
+let edad = prompt ('Tu edad pequeño terricola');
+edad = parseInt(edad)                        
+console.log('Tu edad:' + edad )
+
+saludo = 'Hola ' + nombre + " de " + edad + ' años';
+alert (saludo);
+console.log (saludo)
